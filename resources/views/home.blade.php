0465aff8f1d56b16bbe56f1c35896e3dbc30f33b
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-10 14:55:59 +0200

    add about and contact page and route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap-grid.css' integrity='sha512-hhSu9overYjKfSjPCtJW3688VHkfBh+W1pR5Mysll91bOJwGjYntytGTtVXb2aisFOaYXXDrO38NKXDRPJWu7A==' crossorigin='anonymous'/>
 </head>
 <body>
-    <h1>sono la home</h1>
+    <h1>Home page</h1>
+    <nav>
+        <ul>
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="{{ route('about') }}">About us</a></li>
+            <li><a href="{{ route('contact') }}">Contatti</a></li>
+            
+        </ul>
+    </nav>
 </body>
 </html>
