625184ae69c08fbe68a3acc55be443799d8c1331
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-09-10 15:12:56 +0200

    add bootstrap

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,15 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap-grid.css' integrity='sha512-hhSu9overYjKfSjPCtJW3688VHkfBh+W1pR5Mysll91bOJwGjYntytGTtVXb2aisFOaYXXDrO38NKXDRPJWu7A==' crossorigin='anonymous'/>
+
 </head>
 <body>
-    <h1>About us</h1>
-    <nav>
-        <ul>
-            <li><a href="{{ route('home') }}">Home</a></li>
-            <li><a href="{{ route('about') }}">About us</a></li>
-            <li><a href="{{ route('contact') }}">Contatti</a></li>
-        </ul>
-    </nav>
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-3">
+            <h1>ABOUNT US</h1>
+            </div>
+            <div class="col-3">
+                <nav>
+                    <ul class="d-flex flex-row justify-content-between">
+                        <li><a href="{{ route('home') }}">Home</a></li>
+                        <li><a href="{{ route('about') }}">About us</a></li>
+                        <li><a href="{{ route('contact') }}">Contatti</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,13 +7,21 @@
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap-grid.css' integrity='sha512-hhSu9overYjKfSjPCtJW3688VHkfBh+W1pR5Mysll91bOJwGjYntytGTtVXb2aisFOaYXXDrO38NKXDRPJWu7A==' crossorigin='anonymous'/>
 </head>
 <body>
-    <h1>Home page</h1>
-    <nav>
-        <ul>
-            <li><a href="{{ route('home') }}">Home</a></li>
-            <li><a href="{{ route('about') }}">About us</a></li>
-            <li><a href="{{ route('contact') }}">Contatti</a></li>
-        </ul>
-    </nav>
+<div class="container">
+        <div class="row justify-content-between">
+            <div class="col-3">
+            <h1>HOME PAGE</h1>
+            </div>
+            <div class="col-3">
+                <nav>
+                    <ul class="d-flex flex-row justify-content-between">
+                        <li><a href="{{ route('home') }}">Home</a></li>
+                        <li><a href="{{ route('about') }}">About us</a></li>
+                        <li><a href="{{ route('contact') }}">Contatti</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
